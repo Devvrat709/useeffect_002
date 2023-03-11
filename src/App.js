0d80cba3fbc3eb134a08561b9f1104ac8cffc29f
Dev vrat <devvrat709@gmail.com>
2023-03-12 00:33:42 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [movie, setMovie] = useState([])
+  const [text, setText] = useState("search movie")
+  const changeText = (event) => {
+    setText(event.target.value)
+  }
+  const getMovie = (e) => {
+    e.preventDefault()
+    axios.get(`https://www.omdbapi.com/?s=${text}&apikey=43033444`)
+      .then((res) => {
+        console.log(res)
+        setMovie(res.data.Search)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={text} onChange={changeText} />
+            <button className="btn btn-outline-success" type="submit" onClick={getMovie} >Search</button>
+          </form>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className='row'>
+          {
+            movie.map((value, index) => {
+              return (
+                <div className='col-3' key={index}>
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src={value.Poster} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h3 className="card-title">{value.Year}</h3>
+                      <h4 className="card-text">{value.Title}</h4>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
